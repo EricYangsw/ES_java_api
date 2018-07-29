e9ae5e0b0b3f64de7cded2cf18d1fc12ec3ccc0f
d06521005 <d06521005@ntu.edu.tw>
2018-07-30 00:00:18 +0800

    first

---
+++ src/main/java/tup/es/client/TestClient.java
@@ -0,0 +1,34 @@
+package tup.es.client;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;;
+
+
+public class TestClient 
+{
+    public static String CLUSTER_NAME = "elasticsearch";
+    public static String HOST_IP = "127.0.0.1";
+    public static int TCP_PORT = 9300;
+	public static void main(String[] args) throws UnknownHostException
+	{
+		// Declare settings obj and assigning cluster name
+        Settings settings = Settings.builder().put("cluster.name", CLUSTER_NAME).build();
+        
+        // TransportClient obj to connect with elasticsearch 
+        TransportClient client = new PreBuiltTransportClient(settings)
+        		                    .addTransportAddress(new TransportAddress(
+        				             InetAddress.getByName(HOST_IP), TCP_PORT));
+        
+        // "TransportClient.prepareGet" method to get a document
+        GetResponse getResponse = client.prepareGet("blog", "article", "1").get();
+        
+        System.out.println(getResponse.getSourceAsString());	
+	}//# void
+}//#class
